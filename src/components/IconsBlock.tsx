76c4e9ff29328ff559529de0614e773b25cbfac0
Roman <sselbstmord@gmail.com>
2023-10-24 22:57:21 +0300

    bugs test

--- src/components/IconsBlock.tsx
+++ src/components/IconsBlock.tsx
@@ -1,25 +1,25 @@
 import {Icon} from "./icon/Icon";
 import {SocialLinkBlock, StyledIcon, StyledLink} from "../layout/footer/footericonsblock/FooterIconsBlock";
-import styled from "styled-components";
+
 
 
 export const IconsBlock = () => {
     return (
         <SocialLinkBlock gap={'25px'}>
             <StyledIcon>
-                <StyledLink>
+                <StyledLink href=''>
                     <Icon iconId={'HeaderGitHub'} viewBox={'0 0 33 33'} width={'33'} height={'33'}/>
                 </StyledLink>
             </StyledIcon>
 
             <StyledIcon>
-                <StyledLink>
+                <StyledLink href=''>
                     <Icon iconId={'HeaderLinkedIn'} viewBox={'0 0 33 33'} width={'33'} height={'33'}/>
                 </StyledLink>
             </StyledIcon>
 
             <StyledIcon>
-                <StyledLink>
+                <StyledLink href=''>
                     <Icon iconId={'HeaderTelegram'} viewBox={'0 0 35 30'} width={'35'} height={'30'}/>
                 </StyledLink>
             </StyledIcon>
